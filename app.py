9d33b22647ae342450138f0f596907ab41df8dc5
ikedayuji <93358246+ikedayuji@users.noreply.github.com>
2024-03-25 21:45:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,34 +7,35 @@ class App(ctk.CTk):
         self.configuracoes_da_janela_inicial()
         self.tela_de_login()
     
-    #configurando a janela principal    
+    # Configurando a janela principal    
     def configuracoes_da_janela_inicial(self):
         self.geometry("700x400")
         self.title("Tela de Login")
         self.resizable(False, False)    
 
     def tela_de_login(self):
-        #Trabalhando com as imagens
-        self.img = PhotoImage(file="logi-img.png")
+        # Trabalhando com as imagens
+        self.img = PhotoImage(file="login.png")
         self.img = self.img.subsample(6,6)
+        self.lb_img = ctk.CTkLabel(self, image=self.img)
         self.lb_img.grid(row=1, column=0, padx=10)
         
-        #Titulo da Plataforma
+        # Titulo da Plataforma
         self.title = ctk.CTkLabel(self, text="Faça o seu login ou Cadastre! \nPara ter acesso na plataforma", font=("Century Gothic bold", 14))
         self.title.grid(row=0, column=0, pady=10, padx=10)
         
-        #Criar a frame do formulario de login
-        self.frame_login = ctk.CTKFrame(self, width=350, height=380)
+        # Criar a frame do formulario de login
+        self.frame_login = ctk.CTkFrame(self, width=350, height=380)
         self.frame_login.place(x=350, y=10)
   
-        #Colocando widget dentro do frame - formulario de login
+        # Colocando widget dentro do frame - formulario de login
         self.lbtitle = ctk.CTkLabel(self.frame_login, text="Faça seu Login", font=("Century Gothic bold", 22))
         self.lbtitle.grid(row=0, column=0, padx=10, pady=10)
         
-        self.username_login_entry = ctk.CTkEntry(self.frame_login, width=30, placeholder="Seu nome de usuario...", font=("Century Gothic bold", 16))
+        self.username_login_entry = ctk.CTkEntry(self.frame_login, width=30, font=("Century Gothic bold", 16))
         self.username_login_entry.grid(row=1, column=0, pady=10, padx=10)
   
-        self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=30, placeholder="Sua senha...", font=("Century Gothic bold", 16))
+        self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=30, font=("Century Gothic bold", 16))
         self.senha_login_entry.grid(row=2, column=0, pady=10, padx=10)
     
         self.ver_senha = ctk.CTkCheckBox(self.frame_login, text="Clique para ver a senha", font=("Century Gothic bold", 12))
@@ -50,6 +51,6 @@ class App(ctk.CTk):
         self.btn_cadastro.grid(row=6, column=0, pady=10, padx=10)
         
         
-if __name__ =="__main__":
+if __name__ == "__main__":
     app = App()
     app.mainloop()
