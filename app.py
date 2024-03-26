ca0bdf6b9c2c7f86c0b506dc7dc1b95881efe3b0
ikedayuji <ikedayuji.2002@gmail.com>
2024-03-26 12:54:32 -0300

    Barra do Usuário arrumado

--- app.py
+++ app.py
@@ -32,22 +32,22 @@ class App(ctk.CTk):
         self.lbtitle = ctk.CTkLabel(self.frame_login, text="Faça seu Login", font=("Century Gothic bold", 22))
         self.lbtitle.grid(row=0, column=0, padx=10, pady=10)
         
-        self.username_login_entry = ctk.CTkEntry(self.frame_login, width=30, font=("Century Gothic bold", 16))
+        self.username_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text="Seu nome de usuário...", font=("Century Gothic bold", 16), corner_radius=15, border_color= "#005")
         self.username_login_entry.grid(row=1, column=0, pady=10, padx=10)
   
-        self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=30, font=("Century Gothic bold", 16))
+        self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text="Seu nome de usuário...", font=("Century Gothic bold", 16), corner_radius=15, border_color= "#005")
         self.senha_login_entry.grid(row=2, column=0, pady=10, padx=10)
     
-        self.ver_senha = ctk.CTkCheckBox(self.frame_login, text="Clique para ver a senha", font=("Century Gothic bold", 12))
+        self.ver_senha = ctk.CTkCheckBox(self.frame_login, text="Clique para ver a senha", font=("Century Gothic bold", 12), corner_radius=20)
         self.ver_senha.grid(row=3, column=0, pady=10, padx=10)
         
-        self.btn_login = ctk.CTkButton(self.frame_login, width=30, text="Fazer Login".upper(), font=("Century Gothic bold", 16))
+        self.btn_login = ctk.CTkButton(self.frame_login, width=30, text="Fazer Login".upper(), font=("Century Gothic bold", 16), corner_radius=15)
         self.btn_login.grid(row=4, column=0, pady=10, padx=10)
         
-        self.span = ctk.CTkLabel(self.frame_login, text="Se você não tem conta, clique no botão abaixo para cadastrar!", font=("Century Gothic", 10))
+        self.span = ctk.CTkLabel(self.frame_login, text="Se você não tem conta,\nclique no botão abaixo para cadastrar!", font=("Century Gothic", 10))
         self.span.grid(row=5, column=0, pady=10, padx=10)
 
-        self.btn_cadastro = ctk.CTkButton(self.frame_login, width=300, fg_color="green", hover_color="050", text="Fazer Cadastro".upper(), font=("Century Gothic bold", 16))
+        self.btn_cadastro = ctk.CTkButton(self.frame_login, width=300, fg_color="green", hover_color="#050", text="Fazer Cadastro".upper(), font=("Century Gothic bold", 16))
         self.btn_cadastro.grid(row=6, column=0, pady=10, padx=10)
         
         
