e4fae09fe403b413f53ebad15c270b3879041468
ikedayuji <ikedayuji.2002@gmail.com>
2024-04-04 11:52:10 -0300

    Função de Cadastrar Usuario

--- app.py
+++ app.py
@@ -1,6 +1,66 @@
 import customtkinter as ctk
+from tkinter import *
+import sqlite3
+from tkinter import messagebox
 
-class App(ctk.CTk):
+class BackEnd():
+    def conecta_db(self):
+        self.conn = sqlite3.connect("Sistema_cadastros.db")
+        self.cursor = self.conn.cursor()
+        print("Banco de dados conectado")
+        
+    def desconecta_db(self):
+        self.conn.close()
+        print("Banco de dados desconectado")
+        
+    def cria_tabela(self):
+        self.conecta_db()
+        
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Usuarios (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Username TEXT NOT NULL,
+            Email TEXT NOT NULL,
+            Senha TEXT NOT NULL,
+            Confirma_Senha TEXT NOT NULL
+             )
+        """)
+        
+        try:
+            if (self.username_cadastro == "" or self.email_cadastro=="" or self.senha_cadastro=="" or self.confirma_senha_cadastro==""):
+                messagebox.showerror(title="Sistema de login", message="ERRO!\nPorfavor preencha todos os campos!")
+            elif (len(self.username_cadastro) < 4):
+                messagebox.showwarning(title="Sistema de Login", message="O nome do usuário deve ser pelo menos 4 caracteres.")
+            elif (len(self.senha_cadastro) < 4):
+                messagebox.showwarning(title="Sistema de Login", message="A senha deve ser pelo menos 4 caracteres.")
+            elif (self.senha_cadastro != self.confirma_senha_cadastro):
+                messagebox.showerror(title="Sistema de login", message="ERRO!\nAs senhas que você colocou não estão iguais, coloque novamente as senhas iguais.")
+            else: 
+                self.conn.commit()
+                messagebox.showinfo(title="Sistema de login", message="Parabéns \n{self.username_cadastro}\n Os seus dados foram cadastrados com sucesso")
+                
+        except:
+            messagebox.showerror(title="Sistema de login", message="Erro no processamento do seu cadastro!\nPor favor tente novamente!")
+        
+        
+    def cadastrar_usuario(self):
+        self.username_cadastro = self.username_cadastro_entry.get()
+        self.email_cadastro = self.email_cadastro_entry.get()
+        self.senha_cadastro = self.senha_cadastro_entry.get()
+        self.confirma_senha_cadastro = self.confirma_senha_entry.get()
+        
+        self.conecta_db()
+        
+        self.cursor.execute("""
+            INSERT INTO Usuarios (Username, Email, Senha, Confirma_Senha)
+            VALUES (?, ?, ?, ?)""", (self.username_cadastro, self.email_cadastro, self.senha_cadastro, self.confirma_senha_cadastro))
+
+        self.conn.commit()
+        print("Dados cadastrados com sucesso!")
+        self.desconecta_db()
+
+
+class App(ctk.CTk, BackEnd):
     def __init__(self):
         super().__init__()
         self.configuracoes_da_janela_inicial()
@@ -11,7 +71,8 @@ class App(ctk.CTk):
         self.geometry("700x400")
         self.title("Tela de Login")
         self.resizable(False, False)    
-
+        self.cria_tabela()
+        
     def tela_de_login(self):
         # Criar a frame do formulario de login
         largura_frame = 350
@@ -85,7 +146,7 @@ class App(ctk.CTk):
         self.ver_senha_cadastro = ctk.CTkCheckBox(self.frame_cadastro, text="Clique para ver a senha", font=("Century Gothic bold", 12), corner_radius=20, command=self.mostrar_senha_cadastro)
         self.ver_senha_cadastro.grid(row=5, column=0, pady=10, padx=10)
     
-        self.btn_cadastrar_user = ctk.CTkButton(self.frame_cadastro, width=300, fg_color="green", hover_color="#050", text="Cadastrar".upper(), font=("Century Gothic bold", 16), corner_radius=15)
+        self.btn_cadastrar_user = ctk.CTkButton(self.frame_cadastro, width=300, fg_color="green", hover_color="#050", text="Cadastrar".upper(), font=("Century Gothic bold", 16), corner_radius=15, command=self.cadastrar_usuario)
         self.btn_cadastrar_user.grid(row=6, column=0, pady=10, padx=10)
     
         self.btn_login_back = ctk.CTkButton(self.frame_cadastro, width=30, text="Voltar a Login".upper(), font=("Century Gothic bold", 14), corner_radius=15, fg_color="#444", hover="#333", command=self.voltar_para_login)
@@ -107,6 +168,16 @@ class App(ctk.CTk):
         self.frame_cadastro.place_forget()
         # Exibe o frame de login novamente
         self.tela_de_login()
+        
+    def limpa_entry_cadastro(self):
+        self.username_cadastro_entry.delete(0, END)
+        self.email_cadastro_entry.delete(0, END)
+        self.senha_cadastro_entry.delete(0, END)
+        self.confirma_senha_entry.delete(0, END)
+        
+    def limpa_entry_login(self):
+        self.username_login_entry.delete(0, END)
+        self.senha_login_entry.delete(0, END)
 
 if __name__ == "__main__":
     app = App()
