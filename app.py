b101a3194a7b63abae08bd8d6eda20c268502312
ikedayuji <ikedayuji.2002@gmail.com>
2024-04-06 18:36:03 -0300

    Tela de Login e Cadastro Centralizado

--- app.py
+++ app.py
@@ -28,20 +28,19 @@ class BackEnd():
         
         try:
             if (self.username_cadastro == "" or self.email_cadastro=="" or self.senha_cadastro=="" or self.confirma_senha_cadastro==""):
-                messagebox.showerror(title="Sistema de login", message="ERRO!\nPorfavor preencha todos os campos!")
+                messagebox.showerror(title="Sistema de login", message="ERRO!\nPor favor preencha todos os campos!")
             elif (len(self.username_cadastro) < 4):
-                messagebox.showwarning(title="Sistema de Login", message="O nome do usuário deve ser pelo menos 4 caracteres.")
+                messagebox.showwarning(title="Sistema de Login", message="O nome do usuário deve ter pelo menos 4 caracteres.")
             elif (len(self.senha_cadastro) < 4):
-                messagebox.showwarning(title="Sistema de Login", message="A senha deve ser pelo menos 4 caracteres.")
+                messagebox.showwarning(title="Sistema de Login", message="A senha deve ter pelo menos 4 caracteres.")
             elif (self.senha_cadastro != self.confirma_senha_cadastro):
                 messagebox.showerror(title="Sistema de login", message="ERRO!\nAs senhas que você colocou não estão iguais, coloque novamente as senhas iguais.")
             else: 
                 self.conn.commit()
-                messagebox.showinfo(title="Sistema de login", message="Parabéns \n{self.username_cadastro}\n Os seus dados foram cadastrados com sucesso")
+                messagebox.showinfo(title="Sistema de login", message="\n{self.username_cadastro}\n Os seus dados foram cadastrados com sucesso")
                 self.desconecta_db()
                 self.limpa_entry_cadastro()
         except:
-            messagebox.showerror(title="Sistema de login", message="Erro no processamento do seu cadastro!\nPor favor tente novamente!")
             self.desconecta_db()
         
         
@@ -73,13 +72,13 @@ class BackEnd():
         
         try: 
             if (self.username_login == "" or self.senha_login == ""):
-                messagebox.showwarning(title="Sistema de login", message="Porfavor preencha todos os campos!")
+                messagebox.showwarning(title="Sistema de login", message="Por favor preencha todos os campos!")
             elif (self.username_login in self.verifica_dados and self.senha_login in self.verifica_dados):
-                messagebox.showinfo(title="Sistema de Login", message=f"Parabens {self.username_login}\nLogin feito com sucesso!")
+                messagebox.showinfo(title="Sistema de Login", message=f"Olá {self.username_login}\nSeu login foi feito com sucesso!")
                 self.desconecta_db()
                 self.limpa_entry_login()
         except:
-            messagebox.showerror(title="Sistema de login", message="ERRO!!!\n Dados não encotrados no sistema. Porfavor verifique os seus dados ou cadastre-se no nosso sistema")
+            messagebox.showerror(title="Sistema de login", message="ERRO!!!\n Dados não encontrados no sistema. Por favor verifique os seus dados ou cadastre-se no nosso sistema")
             self.desconecta_db()
         
 class App(ctk.CTk, BackEnd):
@@ -90,100 +89,75 @@ class App(ctk.CTk, BackEnd):
     
     # Configurando a janela principal    
     def configuracoes_da_janela_inicial(self):
-        self.geometry("700x400")
-        self.title("Tela de Login")
-        self.resizable(False, False)    
-        self.cria_tabela()
+        largura_monitor = self.winfo_screenwidth()
+        altura_monitor = self.winfo_screenheight()
         
+        largura_janela = 500
+        altura_janela = 600
+        
+        x_janela = (largura_monitor - largura_janela) // 2
+        y_janela = (altura_monitor - altura_janela) // 2
+        
+        self.geometry(f"{largura_janela}x{altura_janela}+{x_janela}+{y_janela}")
+        self.title("Tela de Login")
+        self.resizable(True, True)    
+        self.cria_tabela()
+        self.update_idletasks()  # Atualiza a geometria da janela antes de exibir
+    
     def tela_de_login(self):
         # Criar a frame do formulario de login
-        largura_frame = 350
-        altura_frame = 380
-        x_frame = (self.winfo_width() - largura_frame) // 2 - 80  
-        y_frame = (self.winfo_height() - altura_frame) // 2 - 40 + 2  
-        self.frame_login = ctk.CTkFrame(self, width=largura_frame, height=altura_frame)
-        self.frame_login.place(x=x_frame, y=y_frame)
+        self.frame_login = ctk.CTkFrame(self, width=400, height=450)
+        self.frame_login.place(relx=0.5, rely=0.5, anchor="center")
   
         # Colocando widget dentro do frame - formulario de login
         self.lbtitle = ctk.CTkLabel(self.frame_login, text="Faça seu Login", font=("Century Gothic bold", 22))
-        self.lbtitle.grid(row=0, column=0, padx=10, pady=10)
+        self.lbtitle.place(relx=0.5, rely=0.1, anchor="center")
         
         self.username_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text="Seu nome de usuário...", font=("Century Gothic bold", 16), corner_radius=15, border_color= "#005")
-        self.username_login_entry.grid(row=1, column=0, pady=10, padx=10)
+        self.username_login_entry.place(relx=0.5, rely=0.25, anchor="center")
   
         self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=300, placeholder_text="Sua senha...", font=("Century Gothic bold", 16), corner_radius=15, border_color= "#005", show="*")
-        self.senha_login_entry.grid(row=2, column=0, pady=5, padx=10)
-        self.senha_visivel = False
-    
-        self.ver_senha_login = ctk.CTkCheckBox(self.frame_login, text="Clique para ver a senha", font=("Century Gothic bold", 12), corner_radius=20, command=self.mostrar_senha_login)
-        self.ver_senha_login.grid(row=3, column=0, pady=10, padx=10)
+        self.senha_login_entry.place(relx=0.5, rely=0.35, anchor="center")
         
         self.btn_login = ctk.CTkButton(self.frame_login, width=30, text="Fazer Login".upper(), font=("Century Gothic bold", 14), corner_radius=15, command=self.verifica_login)
-        self.btn_login.grid(row=4, column=0, pady=10, padx=10)
+        self.btn_login.place(relx=0.5, rely=0.45, anchor="center")
         
         self.span = ctk.CTkLabel(self.frame_login, text="Se você não tem conta,\nclique no botão abaixo para cadastrar!", font=("Century Gothic", 10))
-        self.span.grid(row=5, column=0, pady=10, padx=10)
+        self.span.place(relx=0.5, rely=0.55, anchor="center")
 
         self.btn_cadastro = ctk.CTkButton(self.frame_login, width=300, fg_color="green", hover_color="#050", text="Fazer Cadastro".upper(), font=("Century Gothic bold", 14), corner_radius=15, command=self.tela_de_cadastro)
-        self.btn_cadastro.grid(row=6, column=0, pady=10, padx=10)
-        
-    def mostrar_senha_login(self):
-        if not self.senha_visivel:
-            self.senha_login_entry.configure(show="")
-            self.senha_visivel = True
-        else:
-            self.senha_login_entry.configure(show="*")
-            self.senha_visivel = False
+        self.btn_cadastro.place(relx=0.5, rely=0.65, anchor="center")
         
     def tela_de_cadastro(self):
         #Remover o formulario de login
         self.frame_login.place_forget()
     
         #Frame de formulario de cadastro
-        largura_frame = 350
-        altura_frame = 380
-        x_frame = (self.winfo_width() - largura_frame) // 2 - 80 + 10  
-        y_frame = (self.winfo_height() - altura_frame) // 2 - 40 - 20  
-        self.frame_cadastro = ctk.CTkFrame(self, width=largura_frame, height=altura_frame)
-        self.frame_cadastro.place(x=x_frame, y=y_frame)
+        self.frame_cadastro = ctk.CTkFrame(self, width=400, height=450)
+        self.frame_cadastro.place(relx=0.5, rely=0.5, anchor="center")
     
         #Criando o titulo
         self.lbtitle_cadastro = ctk.CTkLabel(self.frame_cadastro, text="Faça seu Cadastro", font=("Century Gothic bold", 22))
-        self.lbtitle_cadastro.grid(row=0, column=0, padx=10, pady=10)
+        self.lbtitle_cadastro.place(relx=0.5, rely=0.1, anchor="center")
     
         #Criar widgets da tela de cadastro
         self.username_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Seu nome de usuário...", font=("Century Gothic bold", 16), corner_radius=15, border_color= "#005")
-        self.username_cadastro_entry.grid(row=1, column=0, pady=10, padx=10)
+        self.username_cadastro_entry.place(relx=0.5, rely=0.25, anchor="center")
     
         self.email_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Email de usuário...", font=("Century Gothic bold", 16), corner_radius=15, border_color= "#005")
-        self.email_cadastro_entry.grid(row=2, column=0, pady=10, padx=10)
+        self.email_cadastro_entry.place(relx=0.5, rely=0.35, anchor="center")
 
         self.senha_cadastro_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Senha do usuario...", font=("Century Gothic bold", 16), corner_radius=15, border_color= "#005", show="*")
-        self.senha_cadastro_entry.grid(row=3, column=0, pady=10, padx=10)
-        self.senha_visivel_cadastro = False
+        self.senha_cadastro_entry.place(relx=0.5, rely=0.45, anchor="center")
     
         self.confirma_senha_entry = ctk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Confirmar a senha de usuário...", font=("Century Gothic bold", 16), corner_radius=15, border_color= "#005", show="*")
-        self.confirma_senha_entry.grid(row=4, column=0, pady=10, padx=10)
-    
-        self.ver_senha_cadastro = ctk.CTkCheckBox(self.frame_cadastro, text="Clique para ver a senha", font=("Century Gothic bold", 12), corner_radius=20, command=self.mostrar_senha_cadastro)
-        self.ver_senha_cadastro.grid(row=5, column=0, pady=10, padx=10)
+        self.confirma_senha_entry.place(relx=0.5, rely=0.55, anchor="center")
     
         self.btn_cadastrar_user = ctk.CTkButton(self.frame_cadastro, width=300, fg_color="green", hover_color="#050", text="Cadastrar".upper(), font=("Century Gothic bold", 16), corner_radius=15, command=self.cadastrar_usuario)
-        self.btn_cadastrar_user.grid(row=6, column=0, pady=10, padx=10)
+        self.btn_cadastrar_user.place(relx=0.5, rely=0.65, anchor="center")
     
         self.btn_login_back = ctk.CTkButton(self.frame_cadastro, width=30, text="Voltar a Login".upper(), font=("Century Gothic bold", 14), corner_radius=15, fg_color="#444", hover="#333", command=self.voltar_para_login)
-        self.btn_login_back.grid(row=7, column=0, pady=10, padx=10)
-        
-    def mostrar_senha_cadastro(self):
-        if not self.senha_visivel_cadastro:
-            self.senha_cadastro_entry.configure(show="")
-            self.confirma_senha_entry.configure(show="")
-            self.senha_visivel_cadastro = True
-        else:
-            self.senha_cadastro_entry.configure(show="*")
-            self.confirma_senha_entry.configure(show="*")
-            self.senha_visivel_cadastro = False
-
+        self.btn_login_back.place(relx=0.5, rely=0.75, anchor="center")
         
     def voltar_para_login(self):
         # Limpa o frame de cadastro
